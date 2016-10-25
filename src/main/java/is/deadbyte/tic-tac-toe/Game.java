88e713ff04a2574f88ef909daf477f968874a070
Baldur <baldurp12@ru.is>
2016-10-25 14:04:24 +0000

    Added empty classes

---
+++ src/main/java/is/deadbyte/tic-tac-toe/Game.java
@@ -0,0 +1,9 @@
+
+
+public interface Game{
+
+  public void nextTurn(int TableNum)();
+  private void initializeGame();
+  public void nextTurn(int tablePos);
+
+}
---
+++ src/main/java/is/deadbyte/tic-tac-toe/TicTacToe.java
@@ -0,0 +1,10 @@
+
+
+public class TicTacToe implements Game{
+
+  public void nextTurn(int tableNum);
+  private void initializeGame();
+  public void nextTurn(int tablePos);
+
+
+}
