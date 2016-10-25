a9066e1ad688eb8be4468054d0f83d2412f45392
Baldur <baldurp12@ru.is>
2016-10-25 16:50:13 +0000

    Refactoring

--- src/main/java/is/deadbyte/tic-tac-toe/Player.java
+++ src/main/java/is/deadbyte/tic-tac-toe/Player.java
@@ -1,33 +1,20 @@
 import java.util.Scanner;
 
 public class Player {
-    
-	private static String name;
-	private static char symbol;
 
-	public Player() {
-		this.name = "";
-		this.symbol = ' ';
-	}
-	
-	public boolean somePlayerMethod() {
-        return true;
-    }
-	
+	private static String name;
+
+
+	public Player(String name) {
+      
+  }
+
 	public void setName(String n) {
 		name = n;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
-	public void setSymbol(char s) {
-		symbol = s;
-	}
-	
-	public char getSymbol() {
-		return symbol;
-	}
-	
+
 }
--- src/test/java/is/deadbyte/tic-tac-toe/PlayerTest.java
+++ src/test/java/is/deadbyte/tic-tac-toe/PlayerTest.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PlayerTest {
-	
+
 	@Test
 	public void testPlayer() {
         Player player = new Player();
@@ -10,23 +10,14 @@ public class PlayerTest {
         //assertFalse("The initialized state of player returns hasTurn 'false'", player.hasTurn());
         assertTrue("somePlayerMethod should return 'true'", player.somePlayerMethod());
     }
-		
+
 	@Test
 	public void testName() {
 		Player player = new Player();
 
 		player.setName("Gulli");
-		
-		assertEquals("Gulli", player.getName());
-	}
-	
-	@Test
-	public void testSymbol() {
-		Player player1 = new Player();
 
-		player1.setSymbol('X');
-		
-		assertEquals('X', player1.getSymbol());
+		assertEquals("Gulli", player.getName());
 	}
 
 }
