73ca0fe0ba5ba49d151acfaefdfb573258a2e02c
orlygur <orlygurolafs@gmail.com>
2016-10-25 11:02:57 +0000

    Útfært grunn move() fall

--- src/main/java/is/deadbyte/tic-tac-toe/Player.java
+++ src/main/java/is/deadbyte/tic-tac-toe/Player.java
@@ -1,12 +1,17 @@
-
+import java.until.Scanner;
 
 public class Player {
     
 	private static String name;
 	private static char symbol;
 	private static boolean hasTurn;
+	private static int xcord;
+	private static int ycord;
 
 	Player() {
+		this.name = "";
+		this.symbol = '';
+		this.hasTurn = false;
 	}
 	
 	public boolean somePlayerMethod() {
@@ -32,9 +37,44 @@ public class Player {
 	public char getSymbol() {
 		return symbol;
 	}
+
+	public void setXCoord(int x) {
+		this.xcord = x;
+	}
+
+	public int getXCoord() {
+		return this.xcord;
+	}
+
+	public void setYCoord(int y) {
+		this.ycord = y;
+	}
+
+	public int getYcoord() {
+		return this.ycord;
+	}
 	
 	public void move() {
-		hasTurn = false;
+		int xcoord = 5;
+		int ycoord = 5;
+		Scanner input = new Scanner(System.in);
+		if ( this.hasTurn ) {
+			System.out.print("Input 0, 1 or 2 for x coordinates");
+			while (xcoord < 0 && xcoord > 3) {
+				xcoord = input.next();	
+			}
+
+			System.out.print("Input 0, 1 or 2 for y coordinates");
+			while (ycoord < 0 && ycoord > 3) {
+				ycoord = input.next();
+			}
+
+			setXCoord(xcoord);
+			setYCoord(ycoord);
+			hasTurn = false;
+		} else {
+			System.out.print("It is not your move.");
+		}
 	}
 	
 }
--- src/test/java/is/deadbyte/tic-tac-toe/PlayerTest.java
+++ src/test/java/is/deadbyte/tic-tac-toe/PlayerTest.java
@@ -5,9 +5,10 @@ public class PlayerTest {
 	
 	@Test
 	public void testPlayer() {
-        Player classUnderTest = new Player();
+        Player player = new Player();
 
-        assertTrue("somePlayerMethod should return 'true'", classUnderTest.somePlayerMethod());
+        assertFalse("The initialized state of player returns hasTurn 'false'", player.hasTurn());
+        //assertTrue("somePlayerMethod should return 'true'", player.somePlayerMethod());
     }
 	
 	@Test
@@ -20,7 +21,7 @@ public class PlayerTest {
 	}
 	
 	@Test
-	public void testName(){
+	public void testName() {
 		Player player = new Player();
 
 		player.setName("Gulli");
@@ -29,11 +30,18 @@ public class PlayerTest {
 	}
 	
 	@Test
-	public void testSymbol(){
+	public void testSymbol() {
 		Player player1 = new Player();
 
 		player1.setSymbol('X');
 		
 		assertEquals('X', player1.getSymbol());
+	}
+
+	@Test
+	public void testMoev() {
+		Player player = new Player();
+
+		assertEquals("It is not your move.", player.move());
 	}
 }
