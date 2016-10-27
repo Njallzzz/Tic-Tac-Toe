1ff1bf0247dfc47161f124ceaf6e40c649772c34
ivarmarkusson <ivarmar15@ru.is>
2016-10-27 17:41:37 +0000

    Constructor test initialized

--- src/main/java/is/deadbyte/tic-tac-toe/Board.java
+++ src/main/java/is/deadbyte/tic-tac-toe/Board.java
@@ -1,4 +1,15 @@
 public class Board {
+	
+	private char[][] board;
+	
+	Board(int size){
+		
+	}
+	
+	public char[][] getBoard() {
+		return board;
+	}
+	
 	public boolean boardMethood() {
 		return true;
 	}
--- src/test/java/is/deadbyte/tic-tac-toe/BoardTest.java
+++ src/test/java/is/deadbyte/tic-tac-toe/BoardTest.java
@@ -3,10 +3,34 @@ import static org.junit.Assert.*;
 
 
 public class BoardTest {
-
+	
+	private static final int size = 3;
+	
     @Test
 	public void testBoardMethood() {
-        Board board = new Board();
+        Board board = new Board(size);
         assertTrue("boardMethood should return 'true'", board.boardMethood());
+    }
+    
+    @Test
+	public void testBoardConstructor() {
+    	
+    	Board board = new Board(size);
+    	
+    	char[][] testBoard = new char[3][3];
+    	
+    	int counter = 1;
+
+    	for (int i = 0; i < 3; i++)
+    	{
+    		for (int j = 0; j < 3; j++)
+    		{
+    			testBoard[i][j] = (char) counter;
+    			counter++;	
+    		}	
+    	}
+
+    	assertArrayEquals(testBoard, board.getBoard());
+    	
     }
 }
