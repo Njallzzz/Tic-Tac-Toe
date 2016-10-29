faeed912d11ecf9cd1a17e68fafdc54d6b4e7489
hutton85is <hutton85is@yahoo.com>
2016-10-29 18:21:32 +0000

    refactoring test for drawBoard and drawBoard function implemented and tested to work

--- src/main/java/is/deadbyte/tic-tac-toe/ConsoleUI.java
+++ src/main/java/is/deadbyte/tic-tac-toe/ConsoleUI.java
@@ -5,8 +5,17 @@ public class ConsoleUI{
     * @param board contains moves made, and available moves in a two dimensional 
     * array, board[line][column]
     */
-    public void drawBoard(char[][] board)
-    {
+    public void drawBoard(char[][] board){
+        String output = "";
+
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                output += String.valueOf(board[i][j]);
+            }
+            output += "\n";
+        }
+
+        System.out.println(output);
     }
 
 	public static void main(String [] args){
--- src/test/java/is/deadbyte/tic-tac-toe/ConsoleUITest.java
+++ src/test/java/is/deadbyte/tic-tac-toe/ConsoleUITest.java
@@ -12,18 +12,19 @@ public class ConsoleUITest {
 	 	char[][] stats = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
 
 	 	drawTest.drawBoard(stats);
-	 		
-	 	String output = "       |    |    \n"
-	        +"    "+stats[0][0]+"  | "+stats[0][1]+"  | "+stats[0][2]+"\n"
-	        +"   ____|____|____\n"
-	        +"       |    |    \n"
-	        +"    "+stats[1][0]+"  | "+stats[1][1]+"  | "+stats[1][2]+"\n"
-	        +"   ___ |___ |____\n"
-	        +"       |    |    \n"
-	        +"    "+stats[2][0]+"  | "+stats[2][1]+"  | "+stats[2][2]+"\n"
-	        +"       |    |    \n";
 
-	 	assertEquals(output, outContent.toString());
+	 	String outputTest = "";
+
+
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                outputTest += String.valueOf(stats[i][j]);
+            }
+            outputTest += "\n";
+        }
+        outputTest += "\n";
+
+	 	assertEquals(outputTest, outContent.toString());
 	 		
 	 	System.setOut(null);
     	}
