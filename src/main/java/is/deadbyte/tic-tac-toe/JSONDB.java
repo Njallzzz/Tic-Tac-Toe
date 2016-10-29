88a830626e0b4eaa3ae820e0cd5638ef6d5786a5
Baldur <baldurp12@ru.is>
2016-10-29 18:55:26 +0000

    JSON DataBase init

---
+++ src/main/java/is/deadbyte/tic-tac-toe/JSONDB.java
@@ -0,0 +1,35 @@
+
+
+public class JSONDB{
+    File fileName;
+    FileReader reader;
+    FileWriter writer;
+
+    public JSONDB(){
+      fileName = "";
+    }
+
+    public void addWin(String name){
+
+    }
+
+    public void addLoss(String name){
+
+    }
+
+    public void addPlayer(String name){
+
+    }
+
+    public void getStatsWins(String name){
+
+    }
+
+    public void getStatsLosses(String name){
+
+    }
+
+    private boolean isInDataBase(String name{
+      return false;
+    }
+}
--- src/test/java/is/deadbyte/tic-tac-toe/JSONTest.java
+++ src/test/java/is/deadbyte/tic-tac-toe/JSONTest.java
@@ -6,25 +6,13 @@ import static org.junit.Assert.*;
 public class JSONTest{
 
     @Test
-    public void JSONTester(){
-        JSONObject playerTest = new JSONObject();
-              
-        Player player1 = new Player();
-        Player player2 = new Player();
+    public test addWin(){
+      JSONObject compareObject = {"Baldur":[{"Wins":"12"},{"Losses":"0"}]};
         
-        Player[] playerArr = new Player[2];
-        playerArr[0] = player1;
-        playerArr[1] = player2;
-        JSONArray playersJson = new JSONArray(playerArr);
-        
-        playerTest.put("Players", playersJson);
 
-        
-        
-        player1.setName(playerTest.get("name").toString());  
-        
-        assertEquals(player1.getName(), "Baldur");
     }
+
+
 
 
 }
