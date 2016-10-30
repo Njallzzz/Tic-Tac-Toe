920d2b18b7dae777a41b5678ed60029ae07f8054
hutton85is <hutton85is@yahoo.com>
2016-10-30 14:38:48 +0000

    added main function in consoleUI

--- src/main/java/is/deadbyte/tic-tac-toe/ConsoleUI.java
+++ src/main/java/is/deadbyte/tic-tac-toe/ConsoleUI.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ConsoleUI{
 	
-    public void drawBoard(char[][] board){
+    public static void drawBoard(char[][] board){
          String output = "";
 
         for (int i = 0; i < 3; i++){
@@ -14,6 +14,50 @@ public class ConsoleUI{
         }
 
         System.out.println(output);
+    }
+
+    public static void main(String [] args){
+        
+        String nameX, nameO, message, checkWinner;
+
+        System.out.println("WELCOME TO A GAME OF TIC TAC TOE");
+        System.out.println("Enter the name of Player X:");
+        Scanner input = new Scanner(System.in);
+        nameX = input.next();
+        System.out.println("Enter the name of Player O:");
+        nameO = input.next();
+
+        TicTacToe ttt = new TicTacToe(nameX, nameO);
+        nameX = ttt.getPlayerX().getName();
+        nameO = ttt.getPlayerO().getName();
+
+        System.out.println();
+        System.out.println();
+
+        drawBoard(ttt.getBoard());
+
+        while(!ttt.checkWinner().equals("X") && !ttt.checkWinner().equals("O") && !ttt.checkBoardFull()){
+
+            if (ttt.getPlayerXTurn()){
+                System.out.println(nameX + " select a number between 1-9: ");
+            }
+            else{
+                System.out.println(nameO + " select a number between 1-9: ");
+            }
+
+            message = ttt.nextTurn(input.nextInt());
+            System.out.println(message);
+            
+            System.out.println();
+            drawBoard(ttt.getBoard());
+        }
+
+        if(ttt.checkWinner().equals("X"))
+            System.out.println(nameX);
+        else if(ttt.checkWinner().equals("O"))
+            System.out.println(nameO);
+        else
+            System.out.println("DRAW!");
     }
 	
 }
