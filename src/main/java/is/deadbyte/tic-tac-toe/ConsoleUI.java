4c9f4ff92ec73d2ddfe2b1b3e984cb17cb38ba7b
hutton85is <hutton85is@yahoo.com>
2016-10-30 19:04:18 +0000

    Added test cases for rest of classes of console program

---
+++ src/main/java/is/deadbyte/tic-tac-toe/ConsoleUI.java
@@ -0,0 +1,48 @@
+/**
+* <h1>ConsoleUi class</h1>
+* A class that is used to illuminate the game to console
+* <p>
+* @param nameX is the variable for player number one
+* @param nameO is the variable for player number two
+* @param message is variable that is used for entering which
+* square for your move
+*/
+
+import java.util.Scanner;
+import java.io.IOException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+public class ConsoleUI{
+
+    private static String nameX, nameO, message;
+	
+    /**
+    * A method that draws the board to console
+    * @para board is the char board variable
+    */
+    public static void drawBoard(char[][] board){
+        
+    }
+
+     /**
+    * A method that gets user inputs for the 
+    * two players and uses global variables
+    */
+    private static void getUserInputs(){
+    }
+
+    /**
+    * A method that checks if a string is a number (numeric)
+    * @para str is the string that we check
+    * @return true if string is numeric / else false
+    */
+     public static boolean isNumeric(String str){
+        return true;
+    }
+
+
+    public static void main(String [] args){
+    }
+	
+}
---
+++ src/test/java/is/deadbyte/tic-tac-toe/PlayerTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+	@Test
+	public void testPlayer() {
+        Player player = new Player("Baldur");
+
+		player.setName("Baldur");
+
+        assertEquals(player.getName(), "Baldur");
+    }
+
+	@Test
+	public void testName() {
+		Player player = new Player("");
+
+		player.setName("Gulli");
+
+		assertEquals("Gulli", player.getName());
+	}
+
+}
