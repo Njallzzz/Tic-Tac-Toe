13b46ce21b0cd030c47b793af36239e4d59cc193
Baldur <baldurp12@ru.is>
2016-10-30 00:29:03 +0000

    Major refactoring on tests

--- src/test/java/is/deadbyte/tic-tac-toe/JSONDBTest.java
+++ src/test/java/is/deadbyte/tic-tac-toe/JSONDBTest.java
@@ -13,21 +13,29 @@ public class JSONDBTest{
     public JSONDB db;
 
     @Test
+    public void testTest() throws IOException{
+        resetTestBase();
+    }
+
+    @Test
     public void testStatsWins() throws IOException{
       db = new JSONDB();
-      assertEquals(db.getStatsWins("Baldur"), 12);
+      resetTestBase();
+      db.addWin("Tester");
+      assertEquals(db.getStatsWins("Tester"), 11);
     }
 
     @Test
     public void testStatsLosses() throws IOException{
       db = new JSONDB();
-      assertEquals(db.getStatsLosses("Njáll"), 12);
+      resetTestBase();
+      assertEquals(db.getStatsLosses("Tester"), 10);
     }
 
     @Test
     public void addPlayer() throws IOException{
       db = new JSONDB();
-
+      resetTestBase();
       db.addPlayer("Lúðvík");
       assertEquals(db.getStatsWins("Lúðvík"), 0);
 
@@ -37,14 +45,31 @@ public class JSONDBTest{
     public void testAddWin() throws IOException{
       db = new JSONDB();
 
-      assertEquals(db.getStatsWins("Baldur"), 12);
+      resetTestBase();
 
-      db.addWin("Baldur");
+      db.addWin("Tester");
 
-      assertEquals(db.getStatsWins("Baldur"), 13);
-
+      assertEquals(db.getStatsWins("Tester"), 11);
 
     }
 
+    public void resetTestBase() throws IOException{
+      JSONObject fileJson = new JSONObject();
+      JSONArray newUserArr = new JSONArray();
+      JSONObject emptyWins = new JSONObject();
+      emptyWins.put("Wins", "10");
+      JSONObject emptyLosses = new JSONObject();
+      emptyLosses.put("Losses", "10");
+      newUserArr.put(emptyWins);
+      newUserArr.put(emptyLosses);
 
+      fileJson.put("Tester", newUserArr);
+      String writeString = new String(String.valueOf(fileJson));
+
+      // Write to file
+      FileWriter writer = new FileWriter("db/dataBase.json");
+      writer.write(writeString);
+      writer.flush();
+      writer.close();
+    }
 }
